476455ce59590bd816ed278f34ce4ff0abf7aa0a
Otarossoni <otarossoni@gmail.com>
2023-01-25 13:56:28 -0300

    feat: narrowing

---
+++ 4_Narrowing/desafio03/index.ts
@@ -0,0 +1,20 @@
+function userRewiew(review: boolean | number) {
+  if (typeof review === "number") {
+    if (review === 5) {
+      return "Usuário Nota 5";
+    } else if (review === 4) {
+      return "Usuário Nota 4";
+    } else if (review === 3) {
+      return "Usuário Nota 3";
+    } else if (review === 2) {
+      return "Usuário Nota 2";
+    } else if (review === 1) {
+      return "Usuário Nota 1";
+    }
+  } else {
+    return "Sem nota para esse usuário!";
+  }
+}
+
+console.log(userRewiew(1));
+console.log(userRewiew(false));
